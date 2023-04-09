f98e611f2040fa78b68b593831f0819d337c799a
whoareyouwhoami <young.woo@hotmail.com>
2023-04-09 14:43:24 +0900

    백오피스 요청 스키마 수정

--- module-backoffice/src/main/java/com/miridih/library/book/ui/request/BookMetaSearchRequest.java
+++ module-backoffice/src/main/java/com/miridih/library/book/ui/request/BookMetaSearchRequest.java
@@ -2,6 +2,7 @@ package com.miridih.library.book.ui.request;
 
 import com.miridih.library.book.application.dto.BookMetaSearchCondition;
 import com.miridih.library.book.application.dto.ExternalBookMetaSearchCondition;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -23,8 +24,9 @@ public class BookMetaSearchRequest {
         @Schema(description = "도서 이름")
         private String name;
 
-        private PagingInfo pagingInfo = new PagingInfo();
+        private Pagination pagination = new Pagination();
 
+        @Parameter(description = "결과 정렬 기준")
         @Schema(description = "결과 정렬 기준", defaultValue = "REGISTERED_DATE")
         private Sort sort = Sort.REGISTERED_DATE;
 
@@ -34,8 +36,8 @@ public class BookMetaSearchRequest {
                     .bookMetaId(bookMetaId)
                     .name(name)
                     .categoryId(categoryId)
-                    .page(pagingInfo.getPage())
-                    .size(pagingInfo.getSize())
+                    .page(pagination.getPage())
+                    .size(pagination.getSize())
                     .sort(BookMetaSearchCondition.Sort.valueOf(sort.name()))
                     .build();
         }
@@ -45,20 +47,20 @@ public class BookMetaSearchRequest {
     public static class ExternalBookMetaSearchRequest {
         @Schema(description = "도서 이름")
         private String name;
-        private PagingInfo pagingInfo = new PagingInfo();
+        private Pagination pagination = new Pagination();
 
         public ExternalBookMetaSearchCondition toExternalBookMetaSearchCondition() {
             return ExternalBookMetaSearchCondition
                     .builder()
                     .query(name)
-                    .page(pagingInfo.getPage())
-                    .size(pagingInfo.getSize())
+                    .page(pagination.getPage())
+                    .size(pagination.getSize())
                     .build();
         }
     }
 
     @Data
-    private static class PagingInfo {
+    private static class Pagination {
         @Schema(name = "page", description = "패이지 번호", defaultValue = "0")
         private int page = 0;
 
